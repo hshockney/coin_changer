3f1db9cb994045398409ae404042807322f8ba7c
hshockney <shockney@atlanticbb.net>
2015-09-17 16:43:21 -0400

    added test to check for correct change, added colde to make tests pass.

--- coin_changer.rb
+++ coin_changer.rb
@@ -2,17 +2,18 @@ def coin_changer(amount)
  start_values={quarter:0, dime:0, nickel:0, penny:0}
  coin_choices= {quarter:25, dime:10, nickel:5, penny:1}
  
- 
- 
-coin_choices.each do |coin, value|
-  if value==amount
- start_values[coin]+=1
-  end
-  
+ coin_choices.each do |coin, value|
+    while
+    value<=amount
+    start_values[coin]+=1
+    amount=amount-value
+   end 
+end
+
+start_values
+ end
 	
-	end
-	start_values
-end	
+	
 
 #coins={penny:1,nickel:5, dime:10, quarter:25}
   # if value==1
--- coin_changer_test.rb
+++ coin_changer_test.rb
@@ -19,7 +19,22 @@ assert_equal({quarter:1, dime:0, nickel:0, penny:0},coin_changer(25))
 end
 
  def test_for_six_cents
- assert_equal({quarter:0, dime:0, nickel:1, penny:1},coin_changer(5&&1))
+ assert_equal({quarter:0, dime:0, nickel:1, penny:1},coin_changer(5+1))
+ end
+ 
+ def test_for_two_cents
+ assert_equal({quarter:0, dime:0, nickel:0, penny:2},coin_changer(1+1))
+ end
+ def test_for_fifty_five_cents
+ assert_equal({quarter:2, dime:0, nickel:1, penny:0},coin_changer(25+25+5))
+ end
+ 
+ def test_for_sixteen_cents
+ assert_equal({quarter:0, dime:1, nickel:1, penny:1},coin_changer(10+5+1))
+ end
+ 
+ def test_for_three_cents
+ assert_equal({quarter:0, dime:0, nickel:0, penny:3},coin_changer(1+1+1))
  end
 
 
